bef28e47104aa9f2a6bb64c109c347569bd7a9b6
Adriel1221 <36480978+Adriel1221@users.noreply.github.com>
2018-04-10 23:25:39 -0400

    Update Project3.java
    I create a main  planet and add some start to the scene

--- project3/src/project3/Project3.java
+++ project3/src/project3/Project3.java
@@ -1,49 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package project3;
+package creativescene;
 
+import javafx.scene.paint.Color;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
 import javafx.stage.Stage;
 
 /**
  *
- * @author jose.aparicio002
+ * @author Adriel
  */
-public class Project3 extends Application {
+public class CreativeScene extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        Pane pane = new Pane();
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //Creates main planet
+        Ellipse ellipse1 = new Ellipse();
+        ellipse1.centerXProperty().bind(pane.heightProperty().divide(2));
+        ellipse1.centerYProperty().bind(pane.heightProperty().divide(1));
+        ellipse1.setRadiusX(360);
+        ellipse1.setRadiusY(110);
+        ellipse1.setFill(Color.BROWN);
         
-        Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        //Creating stars
+        Circle circle1 = new Circle(60, 60, 2);
+        circle1.setFill(Color.rgb(242, 244, 255, 0.5));
+        Circle circle2 = new Circle(534, 100, 2);
+        circle2.setFill(Color.rgb(242, 244, 255, 0.5));
+        Circle circle3 = new Circle(65, 321, 2);
+        circle3.setFill(Color.rgb(242, 244, 255, 0.5));
+        Circle circle4 = new Circle(365, 248, 2);
+        circle4.setFill(Color.rgb(242, 244, 255, 0.5));
+        Circle circle5 = new Circle(511, 382, 2);
+        circle5.setFill(Color.rgb(242, 244, 255, 0.5));
+        
+        
+        //Adding to the pane
+        pane.getChildren().add(ellipse1);
+        pane.getChildren().add(circle1);
+        pane.getChildren().add(circle2);
+        pane.getChildren().add(circle3);
+        pane.getChildren().add(circle4);
+        pane.getChildren().add(circle5);
+        
+        //Displaying the scene
+        Scene scene = new Scene(pane, 650, 650);
+        scene.setFill(Color.rgb(5,14,60));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
