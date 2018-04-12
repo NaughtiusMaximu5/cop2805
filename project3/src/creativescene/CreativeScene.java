756305ca8c5941ff6cd2517b6370c2313d58cb3a
carlos.hernandez107 <carlos.hernandez107@L340495-2217.mymdc.net>
2018-04-12 19:58:32 -0400

    add smoke

--- project3/src/creativescene/CreativeScene.java
+++ project3/src/creativescene/CreativeScene.java
@@ -1,5 +1,7 @@
 package creativescene;
 
+import java.util.Random;
+import javafx.animation.AnimationTimer;
 import javafx.stage.*;
 import javafx.scene.Scene;
 import javafx.scene.paint.*;
@@ -8,6 +10,8 @@ import javafx.scene.shape.*;
 import javafx.scene.effect.*;
 import javafx.scene.layout.Pane;
 import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
 
 
 public class CreativeScene extends Application {
@@ -16,6 +20,12 @@ public class CreativeScene extends Application {
     public void start(Stage primaryStage) {
         
         Pane pane = new Pane();
+        
+        Smoke smoke = new Smoke(220, 423, Color.WHITESMOKE);
+        Smoke smoke1 = new Smoke(375, 423, Color.WHITESMOKE);
+        
+        smoke.setOpacity(3);
+        smoke1.setOpacity(3);
         
         Ellipse ellipse = new Ellipse();
         DropShadow dropShadow = new DropShadow();
@@ -585,8 +595,8 @@ public class CreativeScene extends Application {
         polygon1.setTranslateY(-67.0);
 
         polygon2.setFill(javafx.scene.paint.Color.valueOf("#fa812a"));
-        polygon2.setLayoutX(395.0);
-        polygon2.setLayoutY(422.0);
+        polygon2.setLayoutX(395.0); 
+        polygon2.setLayoutY(422.0); 
         polygon2.setScaleX(2.0);
         polygon2.setScaleZ(0.0);
         polygon2.setStroke(javafx.scene.paint.Color.BLACK);
@@ -2223,15 +2233,120 @@ public class CreativeScene extends Application {
         pane.getChildren().add(arc12);
         pane.getChildren().add(arc13);
         pane.getChildren().add(arc14);
-
+        pane.getChildren().add(smoke);
+        pane.getChildren().add(smoke1);
 
         /* Display the scene */
         Scene scene = new Scene(pane, 650, 650);
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+        AnimationTimer gameloop = new AnimationTimer() {
+            @Override
+            public void handle(long arg0) {
+                smoke.update();
+                smoke1.update();
+            }
+        };
+        gameloop.start();
     }
+    
     
     public static void main(String[] args) {
         launch(args);
+    }
+}
+
+class Smoke extends Group{
+    WritableImage baseImg;
+    ImageView[] lifeCycle;
+
+    int FRAMES = 50;
+    int FALLING_FRAMES = 32;
+    int width = 60;
+    int height = 60;
+    double radius;
+    Point2D midPoint;
+
+    double xThrow = 0 ; 
+    int xWobble = 1;
+    double yThrow = 5;
+    int yWobble = 10;
+
+    Random random;
+    int throttle;
+    int throttleMax;
+
+    public Smoke(int xLoc, int yLoc, Color color){   
+        throttleMax = 4; // 4 ==> 15 fps animation rate
+        random = new Random();
+
+        midPoint = new Point2D(width/2, height/2);
+        radius = width/2 - 1;
+        lifeCycle = new ImageView[50];
+
+        double topAlpha = 0.5; 
+        double fadeAmount = topAlpha/FALLING_FRAMES;
+        double fadeInAmount = 
+                topAlpha/((FRAMES - FALLING_FRAMES) * 3);
+
+        double pixR = color.getRed();
+        double pixG = color.getGreen();
+        double pixB = color.getBlue();
+        double pixA = topAlpha;
+
+        // Create the base image. 
+        // The color is constant, the alpha is a
+        // computed radial gradient.
+        baseImg = new WritableImage(width, height); 
+        PixelWriter baseRaster = baseImg.getPixelWriter();
+        double distance;
+        double gradientFactor; // between 0 and 1
+
+        for (int jj = 0; jj < height; jj++){
+            for (int kk = 0; kk < width; kk++){
+                distance = midPoint.distance(jj, kk);
+                if (distance > radius) gradientFactor = 0.0;
+                else gradientFactor = (radius - distance) / radius;
+
+                pixA =  topAlpha * gradientFactor;
+                baseRaster.setColor(jj, kk, new Color(pixR, pixG, pixB, pixA));
+            }   
+        }   
+
+        for (int i = 0; i < FRAMES; ++i){
+            lifeCycle[i]= new ImageView();
+            lifeCycle[i].setImage(baseImg);
+            lifeCycle[i].setX(xLoc);
+            lifeCycle[i].setY(yLoc);
+            this.getChildren().add(lifeCycle[i]);
+        }
+
+        // alpha will rise and fall over course of lifeCycle
+        for (int i = 0; i < FALLING_FRAMES; ++i){
+            lifeCycle[i].setOpacity((i + 1) * fadeAmount);
+        }
+
+        for (int i = FALLING_FRAMES; i < FRAMES; i++){
+            lifeCycle[i].setOpacity(
+                    topAlpha - (i - FALLING_FRAMES + 1) * fadeInAmount);
+        }
+    }
+
+    public void update() { 
+        // Slow the animation by skipping cycles. 60 fps is overkill.
+        if (throttle -- > 0) return;
+        else throttle = throttleMax;
+
+        // Propagate location of particles, while adding 
+        //     'throw' and 'wobble'
+        // lifeCycle[n] is stationary (origin of stream, visually)
+        int n = FRAMES - 1; 
+        for (int i = 0; i < n; i++) {
+            lifeCycle[i].setX( lifeCycle[i+1].getX() 
+                + xThrow + random.nextInt(xWobble));
+            lifeCycle[i].setY( lifeCycle[i+1].getY() 
+                + yThrow + random.nextInt(yWobble)); 
+        }       
     }
 }
