27e206e8345626755c60e6535a4be72a2c0c0f97
Abraham <abraham@19cah.com>
2018-07-15 03:06:20 -0400

    Update TestStudents.java

--- project2/src/project2/TestStudents.java
+++ project2/src/project2/TestStudents.java
@@ -1,18 +1,3 @@
-/*-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
-Group Members: Carlos Abraham Hernandez
-               Junior Betancourt
-               Andy Gonzalez
-Student IDs:   4000026424
-               1002223305
-               1000784524
-COP 2805C –Java Programming 2
-Spring 2018 - T Th 6:15 PM - 9:30PM
-Project # 2
-Plagiarism Statement: I certify that this assignment is my own work and that I have not copied in part or
-whole or otherwise plagiarized the work of other students and/or persons.
-
-@author SRC Group
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 package project2;
 
 public class TestStudents {
